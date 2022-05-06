9ce87147cc21a62489d601c63c029dfe5d8dd69b
asadkomi <asadkomi@hotmail.com>
2022-05-06 01:04:25 -0500

    first commit

---
+++ src/theme/fonts.ts
@@ -0,0 +1,18 @@
+const size = {
+  default: 14,
+  xs: 10,
+  s: 12,
+  md: 14,
+  lg: 20,
+  xlg: 24,
+};
+
+const weight = {
+  full: '900',
+  bold: 'bold',
+  normal: 'normal',
+  thin: '400',
+  semi: '600',
+};
+
+export default {size, weight};
